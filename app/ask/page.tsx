7cc84b01edf80c8cfc31803d0e3d88be795e4693
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-07 14:23:02 +0200

    Update page.tsx

--- app/ask/page.tsx
+++ app/ask/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { Suspense } from 'react'
 import Requester from '@/components/requester'
 import style from 'app/page.module.css'
 
