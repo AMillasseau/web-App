8d78147f2bd34c4b075698eab508f88162af85fd
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-07 15:03:03 +0200

    Update page.tsx

--- app/ask/page.tsx
+++ app/ask/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { Suspense } from 'react'
-import Requester from '@/components/requester'
+import Requester from '@/components/table'
 import style from 'app/page.module.css'
 
 export const runtime = 'edge'
