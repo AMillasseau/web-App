fd613034093e7f6cb3caf7817a83089f5148e087
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-07 15:30:30 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,9 @@ export default function Home() {
       </div>
 
       <div className={style.center}>
+       
         <Suspense fallback={<TablePlaceholder />}>
+        {/* @ts-expect-error Async Server Component */}
         <Table />
         </Suspense>
       </div>
