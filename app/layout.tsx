45f4b56098e5c6a708ecaaa04f893c48fb5d6b93
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-07 15:11:31 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font1.className} style={"background: blue"}>{children}</body>
+      <body className={font1.className}>{children}</body>
     </html>
   )
 }
