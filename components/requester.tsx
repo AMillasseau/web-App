3f9cdec167905c00c19637a38d29c6bb92360bd5
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-08 20:16:11 +0200

    Update requester.tsx

--- components/requester.tsx
+++ components/requester.tsx
@@ -1,7 +1,6 @@
 import { sql } from '@vercel/postgres'
 import { timeAgo } from '@/lib/utils'
 import Image from 'next/image'
-import RefreshButton from './refresh-button'
 import { seed } from '@/lib/seed'
 
 export default async function Requester() {
