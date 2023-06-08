9377e6e9065e6c2739b516c7871b7c4f10de3b99
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-08 23:48:11 +0200

    Update requester.tsx

--- components/requester.tsx
+++ components/requester.tsx
@@ -45,8 +45,21 @@ export default async function Requester() {
           <input className={style.ipt} id="name" name="name" type="text" placeholder="Name" autoComplete="off" required/>
           <input className={style.ipt} id="contact" name="contact" type="email" placeholder="Email contact" required/>
           <textarea className={style.txta} id="message" name="text" placeholder="Your message" autoComplete="off" rows={12} required></textarea>
-          <button className={style.btn} id="btn" onClick={() => send(document.getElementById('name').value,document.getElementById('contact').value,document.getElementById('message').value)}> Submit </button>
-          
+  <button
+  className={style.btn}
+  id="btn"
+  onClick={() => {
+    const nameInput = document.getElementById('name');
+    const contactInput = document.getElementById('contact');
+    const messageInput = document.getElementById('message');
+
+    if (nameInput && contactInput && messageInput) {
+      send(nameInput.value, contactInput.value, messageInput.value);
+    }
+  }}
+>
+  Submit
+</button>
     </div>
   )
 }
