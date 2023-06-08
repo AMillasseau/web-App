e33bd5e66618d96454f1cabbacb7af3633addc17
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-08 14:02:08 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  reactStrictMode: true,
-    i18n,
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'src/styles')],
-        prependData: `@import "variables.scss";`,
-    },
     images: {
         remotePatterns: [
             {
