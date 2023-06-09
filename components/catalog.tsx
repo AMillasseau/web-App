943d00f961f89c87269e033d3700617b1bdd19b1
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-09 17:41:41 +0200

    Update catalog.tsx

--- components/catalog.tsx
+++ components/catalog.tsx
@@ -45,7 +45,7 @@ function Dispo({ booked, name }: { booked: boolean; name: string }) {
 }
 
 export default async function Table() {
-   const connectionString = process.env.POSTGRES_URL;
+   const connectionString = "Server=ep-proud-field-232095-pooler.us-east-1.postgres.vercel-storage.com;Database=verceldb;User Id=default;Password=oTM3KYNDsWk5;";
       let data;
 
       try {
