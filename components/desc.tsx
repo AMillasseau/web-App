f6814bf3465f12e9a84223c214111e193227b7c9
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-09 13:12:06 +0200

    Update desc.tsx

--- components/desc.tsx
+++ components/desc.tsx
@@ -49,7 +49,7 @@ async function booking(name: string){
   }
   
 
-export default async function Table({params: { name: string }}) {
+export default async function Table({ params: { name } }: { params: { name: string } }) {
   const { name } = params;
   let data
   let query = sql`SELECT * FROM games WHERE name = ${name} LIMIT 1`;
