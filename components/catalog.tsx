5ad07eed5f64e9234dd20a37b5d0be9dd1ecd925
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-09 13:48:22 +0200

    Update catalog.tsx

--- components/catalog.tsx
+++ components/catalog.tsx
@@ -1,80 +1,88 @@
-import { sql } from '@vercel/postgres'
-import { timeAgo } from '@/lib/utils'
-import Image from 'next/image'
-import { seed } from '@/lib/seed'
-import React from 'react'
+// Table Client Component
 
-import style from '@/app/page.module.css'
+import React from 'react';
+import { sql } from '@vercel/postgres';
+import { seed } from '@/lib/seed';
+import Image from 'next/image';
+import style from '@/app/page.module.css';
 
-function imag(url: string) {
-  if (url == ''){
-    return (<div> </div>)}
-  else {
-    return (<Image alt='' src={url} height={100}/>)}
+function Imag({ url }: { url: string }) {
+  if (url === '') {
+    return <div></div>;
+  } else {
+    return <Image alt="" src={url} height={100} />;
+  }
 }
 
-function dispo(d: boolean, name: string) {
+function Dispo({ booked, name }: { booked: boolean; name: string }) {
   const handleBooking = async () => {
-    let data
-  let query = sql`UPDATE games SET booked = true WHERE name = ${name}`
-  try {
-    data = await query
-  } catch (e: any) {
-    if (e.message === `relation "games" does not exist`) {
-      console.log(
-        'Table does not exist, creating and seeding it with dummy data now...'
-      )
-      // Table is not created yet
-      await seed()
-      data = await query
-    } else {
-      throw e
+    let data;
+    let query = sql`UPDATE games SET booked = true WHERE name = ${name}`;
+    try {
+      data = await query;
+    } catch (e: any) {
+      if (e.message === `relation "games" does not exist`) {
+        console.log(
+          'Table does not exist, creating and seeding it with dummy data now...'
+        );
+        // Table is not created yet
+        await seed();
+        data = await query;
+      } else {
+        throw e;
+      }
     }
-  }
   };
 
-  if (d) {
+  if (booked) {
     return <button type="button" disabled>Already booked</button>;
   } else {
     return <button type="button" onClick={handleBooking}>Book</button>;
   }
 }
 
+export default function Table() {
+  const [games, setGames] = React.useState([]);
 
-export default async function Table() {
-  let data
+  React.useEffect(() => {
+    async function fetchData() {
+      let data;
 
-  try {
-    data = await sql`SELECT * FROM games`
-  } catch (e: any) {
-    if (e.message === `relation "games" does not exist`) {
-      console.log(
-        'Table does not exist, creating and seeding it with dummy data now...'
-      )
-      // Table is not created yet
-      await seed()
-      data = await sql`SELECT * FROM games`
-    } else {
-      throw e
+      try {
+        data = await sql`SELECT * FROM games`;
+      } catch (e: any) {
+        if (e.message === `relation "games" does not exist`) {
+          console.log(
+            'Table does not exist, creating and seeding it with dummy data now...'
+          );
+          // Table is not created yet
+          await seed();
+          data = await sql`SELECT * FROM games`;
+        } else {
+          throw e;
+        }
+      }
+
+      const { rows } = data;
+      setGames(rows);
     }
-  }
 
-  const { rows: games } = data
- 
-  return (      
+    fetchData();
+  }, []);
+
+  return (
     <div className={style.catdiv}>
-      <p className={style.bigtxt}> Catalog  </p>
-        {games.map((game) => (
-          <div key={game.name} className={style.catcard1}>
-              {imag(game.img)}
-            <div className={style.catcard2}>
-            <p> {game.name} </p>
-              {dispo(game.booked,game.name)}
-              <p> {game.description} </p>
-            </div> 
-          </div> 
-          )
-        )}
+      <p className={style.bigtxt}>Catalog</p>
+      {games.map((game: any) => (
+        <div key={game.name} className={style.catcard1}>
+          <Imag url={game.img} />
+          <div className={style.catcard2}>
+            <p>{game.name}</p>
+            <Dispo booked={game.booked} name={game.name} />
+            <p>{game.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
