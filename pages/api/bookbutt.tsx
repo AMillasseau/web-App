c86412a13131ccff930bc933e6ee8b6fc01c03ec
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-13 13:56:35 +0200

    Create bookbutt.tsx

---
+++ pages/api/bookbutt.tsx
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export default async function handle(req, res) {
+    const data = req.body;
+    const bid = data.id;
+    const user = await prisma.games.findUnique({ where: { id: bid } });
+    if (user !== null) {
+      user.booked = true;
+      const updatedUser = await prisma.games.update({
+        where: { id: user.id },
+        data: { name: user.name },
+      });
+    res.json(bid)
+}
