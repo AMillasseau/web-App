deb4a94480f27c41ef3fbab48e496e21a84d626d
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-13 14:54:29 +0200

    Update catalog.tsx

--- components/catalog.tsx
+++ components/catalog.tsx
@@ -18,7 +18,7 @@ const prisma = new PrismaClient()
   }
 }
 
-async function fetcher(url) {
+async function fetcher(url : string) {
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error('An error occurred while fetching the data.');
