f2e389313d6661c22baae43d51eea0249c5f23e1
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-14 12:40:39 +0000

    modified:   pages/api/gamelist.tsx

--- pages/api/gamelist.tsx
+++ pages/api/gamelist.tsx
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-export default async function handle(req, res) {
+export default async function handle(req : any, res : any) {
     const gamelist = await prisma.games.findMany();
     res.json(gamelist)
 }
