4edc2007aa9dc7212063043378bfe6dd5a9ef4da
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-15 13:05:11 +0200

    Update catalog.js

--- components/catalog.js
+++ components/catalog.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import Image from 'next/image';
 import style from '@/app/page.module.css';
-const { sql } = require('pg-extra');
+import { sql } from '@vercel/postgres'
 
 import { PrismaClient } from '@prisma/client'
 
